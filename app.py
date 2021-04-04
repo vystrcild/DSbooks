502ef7aedb1a5a7a8e536eb1bf26c37fcc1fd893
vystrcild <vystrcil.dusan@gmail.com>
2021-04-04 17:49:10 +0200

    Created detail page for book

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template, request
 from search import SearchForm, get_books_by_search
 from import_data import books
+from get_recs import get_books_by_correlations
 
 app = Flask(__name__)
 
@@ -14,12 +15,16 @@ def index():
 def search_result():
     form = SearchForm(request.form)
     results = get_books_by_search(form.search.data)
-    return render_template("result.html", form=form, results=results)
+    results_len = len(results.index)
+    return render_template("result.html", form=form, results=results, results_len=results_len)
 
 @app.route('/detail/<isbn>', methods=["GET","POST"])
 def get_detail(isbn):
+    form = SearchForm()
     book = books[books["ISBN"]==isbn]
-    return render_template("detail.html", book=book)
+    recommended_books = get_books_by_correlations(isbn)
+    results_len = len(recommended_books.index)
+    return render_template("detail.html", book=book, recs=recommended_books, form=form, results_len=results_len)
 
 if __name__ == '__main__':
     app.run(debug=True)
