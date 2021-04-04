1e97c0c7364fa32f1e69c3489c34ce2792cb4e59
vystrcild <vystrcil.dusan@gmail.com>
2021-04-04 13:32:20 +0200

    Cleanup import_data.py

--- import_data.py
+++ import_data.py
@@ -2,21 +2,19 @@ import pandas as pd
 import numpy as np
 
 # load ratings
-ratings = pd.read_csv('BX-Book-Ratings.csv', encoding='latin-1', sep=';', na_values=0, dtype={'ISBN': 'string'})
+ratings = pd.read_csv('BX-Book-Ratings.csv', encoding='latin-1', sep=';', na_values=0)
 ratings = ratings.dropna()
 
 # load books
-books = pd.read_csv('BX-Books.csv', encoding='latin-1', sep=';', error_bad_lines=False, dtype={'ISBN': 'string'})
-books = books[["Book-Title","ISBN"]]
+books = pd.read_csv('BX-Books.csv', encoding='latin-1', sep=';', error_bad_lines=False)
 
 # Merging ratings & books
 df = pd.merge(ratings, books, on=["ISBN"])
 
-# Create no. of ratings column
+# Create no. of ratings and average columns
 df["Rating-Count"] = df.groupby(['ISBN'])["Book-Rating"].transform(np.sum)
 df["Rating-Average"] = df.groupby(['ISBN'])["Book-Rating"].transform(np.mean)
 
 # Create final DF for books & drop duplicates
 books = df[["Book-Title", "ISBN", "Rating-Count", "Rating-Average"]]
-#books.drop_duplicates(inplace=True)
-#print(books.sort_values("Rating-Count", ascending=False).head())
+books.drop_duplicates(inplace=True)
